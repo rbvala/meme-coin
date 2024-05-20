09329543949ec1955de1ac5c9a9ef8b58ae6dd94
rbvala <rb@vala.finance>
2024-05-20 04:36:08 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     networks: {
         linea: {
             url: "https://rpc.linea.build", // Linea RPC URL
-            accounts: ["0939f2045d8c1b9856e99a6c4c1ad6ea57bc2117e1b2f8fc1dc4bc7778fa3c2f"] // Replace with your private key
+            accounts: ["--------"] // 
         }
     }
 };
